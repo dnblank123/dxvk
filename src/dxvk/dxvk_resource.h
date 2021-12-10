5aedf221a29b96ab92a366893a7faebf58a51dca
Juuso Alasuutari <juuso.alasuutari@gmail.com>
2021-12-10 15:56:31 +0200

    [dxvk] Remove unused functions

--- src/dxvk/dxvk_resource.h
+++ src/dxvk/dxvk_resource.h
@@ -32,11 +32,9 @@ namespace dxvk {
      * the resource by the GPU matching the given access
      * type. Note that checking for reads will also return
      * \c true if the resource is being written to.
-     * \param [in] access Access type to check for
+     * \tparam Access Access type to check for
      * \returns \c true if the resource is in use
      */
-    inline bool isInUse(DxvkAccess access) const;
-    
     template<DxvkAccess Access = DxvkAccess::Read>
     inline bool isInUse() const;
 
@@ -44,13 +42,8 @@ namespace dxvk {
      * \brief Acquires resource
      * 
      * Increments use count for the given access type.
-     * \param Access Resource access type
+     * \tparam Access Resource access type
      */
-    void acquire(DxvkAccess access) {
-      if (access != DxvkAccess::None)
-        m_useCount.u32[to_int(access)].fetch_add(1, std::memory_order_acquire);
-    }
-
     template<DxvkAccess Access>
     inline void acquire();
 
@@ -58,29 +51,20 @@ namespace dxvk {
      * \brief Releases resource
      * 
      * Decrements use count for the given access type.
-     * \param Access Resource access type
+     * \param [in] access Resource access type
      */
     void release(DxvkAccess access) {
       if (access != DxvkAccess::None)
         m_useCount.u32[to_int(access)].fetch_sub(1, std::memory_order_release);
     }
 
-    template<DxvkAccess Access>
-    inline void release();
-
     /**
      * \brief Waits for resource to become unused
      *
      * Blocks calling thread until the GPU finishes
      * using the resource with the given access type.
-     * \param [in] access Access type to check for
+     * \tparam Access Access type to check for
      */
-    void waitIdle(DxvkAccess access) const {
-      sync::spin(50000, [this, access] {
-        return !isInUse(access);
-      });
-    }
-    
     template<DxvkAccess Access>
     inline void waitIdle() const;
 
@@ -106,12 +90,6 @@ namespace dxvk {
     return m_useCount.u32[u_v<DxvkAccess::Write>].load(std::memory_order_acquire) != 0;
   }
 
-  inline bool DxvkResource::isInUse(DxvkAccess access) const {
-    return access == DxvkAccess::Read
-      ? isInUse<DxvkAccess::Read>()
-      : isInUse<DxvkAccess::Write>();
-  }
-
   /* acquire() specialized for DxvkAccess::Read and DxvkAccess::Write */
   template<DxvkAccess Access>
   inline void DxvkResource::acquire() {
@@ -120,15 +98,6 @@ namespace dxvk {
 
   /* acquire() specialized for DxvkAccess::None (no-op) */
   template<> inline void DxvkResource::acquire<DxvkAccess::None>() {}
-
-  /* release() specialized for DxvkAccess::Read and DxvkAccess::Write */
-  template<DxvkAccess Access>
-  inline void DxvkResource::release() {
-    m_useCount.u32[u_v<Access>].fetch_sub(1, std::memory_order_release);
-  }
-
-  /* release() specialized for DxvkAccess::None (no-op) */
-  template<> inline void DxvkResource::release<DxvkAccess::None>() {}
 
   template<DxvkAccess Access>
   inline void DxvkResource::waitIdle() const {
